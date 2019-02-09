71e8c513cb66016a3f3c1fcdb6c37e51e3f57d1e
Quang Truong Pham Ngo <ngotruongphamquang@gmail.com>
2019-02-09 19:58:10 +0700

    Init repo with some code

---
+++ build/precache-manifest.6d036c35241b1b1a219c6d3e9205463f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71b4c95eb0ed6028ff9f",
+    "url": "/static/js/main.71b4c95e.chunk.js"
+  },
+  {
+    "revision": "06af4042aef79c9b4b65",
+    "url": "/static/js/1.06af4042.chunk.js"
+  },
+  {
+    "revision": "3555c74da99ced0ec41d19221b51d36d",
+    "url": "/index.html"
+  }
+];
